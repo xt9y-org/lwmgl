050c2abe04742a5d85f810f7c59636705849008d
xt9y <felix@xt9y.de>
2026-09-08 10:52:33 +0200

    lwmgl: define private Metal context state

---
+++ src/internal.h
@@ -0,0 +1,35 @@
+#ifndef LWMGL_INTERNAL_H
+#define LWMGL_INTERNAL_H
+
+#include <lwmgl/lwmgl.h>
+
+#ifdef __OBJC__
+#import <AppKit/AppKit.h>
+#import <Metal/Metal.h>
+#import <QuartzCore/CAMetalLayer.h>
+#endif
+
+void lwmglSetErrorInternal(const char *message);
+
+#ifdef __OBJC__
+typedef struct LWMGLContextState {
+    __strong id<MTLDevice> device;
+    __strong id<MTLCommandQueue> queue;
+    void *glfwWindow;
+    __unsafe_unretained NSWindow *nsWindow;
+    __unsafe_unretained NSView *view;
+    __strong CAMetalLayer *layer;
+    __strong CALayer *previousLayer;
+    BOOL previousWantsLayer;
+    __strong id<CAMetalDrawable> drawable;
+    int created;
+} LWMGLContextState;
+
+LWMGLContextState *lwmglContextState(void);
+int lwmglSurfaceAttach(void *nativeWindow);
+void lwmglSurfaceDetach(void);
+int lwmglSurfaceAcquireDrawable(void);
+void lwmglSurfaceUpdateDrawableSize(void);
+#endif
+
+#endif
