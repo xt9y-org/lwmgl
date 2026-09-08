e999694190811d6ddc5b40702578ba1ce5a5b8b7
xt9y <felix@xt9y.de>
2026-09-08 11:17:10 +0200

    lwmgl: define command handle

---
+++ include/lwmgl/command.h
@@ -0,0 +1,16 @@
+#ifndef LWMGL_COMMAND_H
+#define LWMGL_COMMAND_H
+
+#include <lwmgl/shader.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct LWMGLCommandImpl *LWMGLCommand;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
