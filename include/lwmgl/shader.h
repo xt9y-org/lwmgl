f2cae3c752dbf18637132f3991d64d757137ff93
xt9y <felix@xt9y.de>
2026-09-08 11:13:21 +0200

    lwmgl: define shader and pipeline handles

---
+++ include/lwmgl/shader.h
@@ -0,0 +1,20 @@
+#ifndef LWMGL_SHADER_H
+#define LWMGL_SHADER_H
+
+#include <lwmgl/texture.h>
+#include <stddef.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct LWMGLLibraryImpl *LWMGLLibrary;
+typedef struct LWMGLFunctionImpl *LWMGLFunction;
+typedef struct LWMGLComputePipelineImpl *LWMGLComputePipeline;
+typedef struct LWMGLRenderPipelineImpl *LWMGLRenderPipeline;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
