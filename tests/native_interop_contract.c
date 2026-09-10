d74cb8ef7cfc2f3ce1b344c51c4ede07821acfa8
xt9y <felix@xt9y.de>
2026-09-10 02:37:10 +0200

    test: require native Metal interop handles

---
+++ tests/native_interop_contract.c
@@ -0,0 +1,10 @@
+#include <lwmgl/lwmgl.h>
+
+int main(void)
+{
+    (void)Metal.nativeDevice;
+    (void)Metal.nativeCommandBuffer;
+    (void)Metal.nativeRenderEncoder;
+    (void)Metal.nativeRenderPassDescriptor;
+    return 0;
+}
